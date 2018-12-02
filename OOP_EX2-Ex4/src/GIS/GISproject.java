6748234704ed2c0fb3e34a16fef2b3ea00af2b1f
שלהבת <shalhevetgamliel@gmail.com>
2018-12-02 22:06:19 +0200

    change gis

--- OOP_EX2-Ex4/src/GIS/GISproject.java
+++ OOP_EX2-Ex4/src/GIS/GISproject.java
@@ -1,113 +1,118 @@
 package GIS;
+
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
+
 public class GISproject implements GIS_project{
-	private MetaDataGroup Metadata;
-	Set<GIS_layer> layers;
+	
+	private MetaDataGroup Meta_data;
+	Set<GIS_layer> layer;
+	
 	public GISproject() {
-		layers = new LinkedHashSet<GIS_layer>();
-		Metadata = new MetaDataGroup();
+		layer = new LinkedHashSet<GIS_layer>();
+		Meta_data = new MetaDataGroup();
 	}
-	Set<GIS_layer> _project= new HashSet<GIS_layer>();
+	
+	Set<GIS_layer> project= new HashSet<GIS_layer>();
 /**
  * add layer to project
  */
 	@Override
 	public boolean add(GIS_layer e) {
-		return _project.add(e);
+		return project.add(e);
 	}
 /**
  * add all to project
  */
 	@Override
 	public boolean addAll(Collection<? extends GIS_layer> c) {
-		return _project.addAll(c);
+		return project.addAll(c);
 	}
 /**
  * clear project
  */
 	@Override
 	public void clear() {
-		_project.clear();
+		project.clear();
 	}
 /**
  * ask if contains o in the project
  */
 	@Override
 	public boolean contains(Object o) {
-		return _project.contains(o);
+		return project.contains(o);
 	}
 /**
  * ask if the collection contains c
  */
 	@Override
 	public boolean containsAll(Collection<?> c) {
-		return _project.containsAll(c);
+		return project.containsAll(c);
 	}
 /**
  * ask if project is empty
  */
 	@Override
 	public boolean isEmpty() {
-		return _project.isEmpty();
+		return project.isEmpty();
 	}
 /**
  * bring the iterator to project
  */
 	@Override
 	public Iterator<GIS_layer> iterator() {
-		return _project.iterator();
+		return project.iterator();
 	}
 /**
  * remove o from project
  */
 	@Override
 	public boolean remove(Object o) {
-		return _project.remove(o);
+		return project.remove(o);
 	}
 /**
  * remove all c from project
  */
 	@Override
 	public boolean removeAll(Collection<?> c) {
-		return _project.removeAll(c);
+		return project.removeAll(c);
 	}
 /**
  * retain all c in project
  */
 	@Override
 	public boolean retainAll(Collection<?> c) {
-		return _project.retainAll(c);
+		return project.retainAll(c);
 	}
 /**
  * return size of project
  */
 	@Override
 	public int size() {
-		return _project.size();
+		return project.size();
 	}
 /**
  * take the _project to array
  */
 	@Override
 	public Object[] toArray() {
-		return _project.toArray();
+		return project.toArray();
 	}
 /**
  * create array from _project
  */
 	@Override
 	public <T> T[] toArray(T[] a) {
-		return _project.toArray(a);
+		return project.toArray(a);
 	}
 /**
  * return the metadata of project
  */
 	@Override
 	public Meta_data get_Meta_data() {       
-	return Metadata;
+	return Meta_data;
 	}
 }
