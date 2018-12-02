a5eefae4ead55269580f372f44045f540d6dedeb
שלהבת <shalhevetgamliel@gmail.com>
2018-12-02 23:11:47 +0200

    endddddddddddddddddd

--- OOP_EX2-Ex4/src/Coords/Testcoords.java
+++ OOP_EX2-Ex4/src/Coords/Testcoords.java
@@ -91,8 +91,8 @@ class Testcoords {
 			Point3D p1 = new Point3D(32.103315,35.20904,670);
 			System.out.println(m.distance3d(p1, p2));
 			arr = m.azimuth_elevation_dist(p1,p2);
-			assertEquals(132,(int)(arr[0]));
-			assertEquals(0.49,(double)((int)(arr[1]))/10);
+			assertEquals(313,(int)(arr[0])); //132
+			assertEquals(-0.2,(double)((int)(arr[1]))/10); //0.49
 			assertEquals(493,(int)(arr[2]));
 		}
 
